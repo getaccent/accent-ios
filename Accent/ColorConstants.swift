2a2b440748d13ef6f7c4446c503c01128b968a64
Jack Cook <hello@jackcook.nyc>
2016-04-09 14:58:54 -0400

    Created an onboarding view controller

---
+++ Accent/ColorConstants.swift
@@ -0,0 +1,24 @@
+//
+//  ColorConstants.swift
+//  Accent
+//
+//  Created by Jack Cook on 4/9/16.
+//  Copyright © 2016 Tiny Pixels. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func accentLightColor() -> UIColor {
+        return UIColor(red: 228/255, green: 237/255, blue: 242/255, alpha: 1)
+    }
+    
+    class func accentDarkColor() -> UIColor {
+        return UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1)
+    }
+    
+    class func accentBlueColor() -> UIColor {
+        return UIColor(red: 0, green: 162/255, blue: 234/255, alpha: 1)
+    }
+}
---
+++ Accent/OnboardingViewController.swift
@@ -0,0 +1,61 @@
+//
+//  OnboardingViewController.swift
+//  Accent
+//
+//  Created by Jack Cook on 4/9/16.
+//  Copyright © 2016 Tiny Pixels. All rights reserved.
+//
+
+import DigitsKit
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var logoLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var digitsButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Digits.sharedInstance().logOut()
+        
+        navigationController?.navigationBarHidden = true
+        
+        view.backgroundColor = UIColor.accentLightColor()
+        
+        logoLabel.textColor = UIColor.accentDarkColor()
+        descriptionLabel.textColor = UIColor.accentDarkColor()
+        
+        digitsButton.setTitleColor(UIColor.accentLightColor(), forState: .Normal)
+        digitsButton.backgroundColor = UIColor.accentDarkColor()
+        digitsButton.layer.cornerRadius = 4
+        
+        skipButton.setTitleColor(UIColor.accentDarkColor(), forState: .Normal)
+        skipButton.layer.borderColor = UIColor.accentDarkColor().CGColor
+        skipButton.layer.borderWidth = 1
+        skipButton.layer.cornerRadius = 4
+    }
+    
+    @IBAction func digitsButtonPressed(sender: UIButton) {
+        let digits = Digits.sharedInstance()
+        let configuration = DGTAuthenticationConfiguration(accountFields: .DefaultOptionMask)
+        configuration.appearance = DGTAppearance()
+        
+        configuration.appearance.accentColor = UIColor.accentDarkColor()
+        
+        digits.authenticateWithViewController(nil, configuration: configuration) { (session, error) in
+            guard let _ = session else {
+                print(error.localizedDescription)
+                return
+            }
+            
+            self.performSegueWithIdentifier("languageSegue", sender: self)
+        }
+    }
+    
+    @IBAction func skipButtonPressed(sender: UIButton) {
+        performSegueWithIdentifier("languageSegue", sender: self)
+    }
+}
