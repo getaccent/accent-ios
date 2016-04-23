86cf1e3ece91774d4520e4aed53477f76e4f60d9
Jack Cook <hello@jackcook.nyc>
2016-04-23 12:59:02 -0700

    Updated article model

--- Accent/Article.swift
+++ Accent/Article.swift
@@ -10,8 +10,36 @@ import RealmSwift
 
 class Article: Object {
     
-    dynamic var articleUrl: String = ""
-    dynamic var imageUrl: String?
-    dynamic var text: String = ""
+    dynamic var url: String = ""
     dynamic var title: String = ""
+    dynamic var image: String = ""
+    dynamic var text: String = ""
+    dynamic var authors: String = ""
+    dynamic var date: Int = 0
+    dynamic var featured: Int = 0
+    dynamic var language: String = ""
+    
+    func getURL() -> NSURL? {
+        return NSURL(string: url)
+    }
+    
+    func getImageURL() -> NSURL? {
+        return NSURL(string: image)
+    }
+    
+    func getAuthors() -> [String]? {
+        return authors.componentsSeparatedByString(",")
+    }
+    
+    func getDate() -> NSDate? {
+        return NSDate(timeIntervalSince1970: NSTimeInterval(date))
+    }
+    
+    func getFeatured() -> Bool {
+        return featured == 1
+    }
+    
+    func getLanguage() -> Language? {
+        return Language.languageFromCode(language)
+    }
 }
